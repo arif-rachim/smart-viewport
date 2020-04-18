b3b27c1f7aae89983788c5372c67dd7a4881e3e3
arif rachim <a.arif.r@gmail.com>
2020-04-18 10:34:05 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DEFAULT_STATE = {
+    contentWidth : 320,
+    contentHeight : 640,
+    viewPortWidth : 320,
+    viewPortHeight : 320
+};
+
+const reducer = (state,action) => {
+    return state;
+};
+
+function fillContentInViewPort(content, viewPort) {
+    // viewPort is always the right position !
+    return undefined;
 }
 
-export default App;
+export default function App(){
+    const [state,dispatch] = useReducer(reducer,DEFAULT_STATE);
+    const content = {width:state.contentWidth,height:state.contentHeight};
+    const viewPort = {width:state.viewPortWidth,height:state.viewPortHeight};
+    const newContent = fillContentInViewPort(content,viewPort);
+    return <div style={{width:'100vw',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center'}}>
+        <div className={'viewport'} style={{width:state.viewPortWidth,height:state.viewPortHeight}} >
+            <div className={'content'} style={{width:state.contentWidth,height:state.contentHeight}}></div>
+        </div>
+    </div>
+}
